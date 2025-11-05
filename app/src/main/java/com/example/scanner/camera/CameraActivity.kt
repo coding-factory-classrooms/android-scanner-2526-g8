f49a05b3f2124b52fc38063109635cd14967f2d4
Kaylana1968 <144374760+Kaylana1968@users.noreply.github.com>
2025-11-05 15:03:17 +0100

    api call working

--- app/src/main/java/com/example/scanner/camera/CameraActivity.kt
+++ app/src/main/java/com/example/scanner/camera/CameraActivity.kt
@@ -1,2 +1,28 @@
 package com.example.scanner.camera
 
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.ui.Modifier
+import com.example.scanner.ui.theme.ScannerTheme
+
+class CameraActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ScannerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Column(Modifier.padding(innerPadding)) {
+                        CameraScreen()
+                    }
+                }
+            }
+        }
+    }
+}
